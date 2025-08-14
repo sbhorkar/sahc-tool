4bf37fd6b501fa004069942338814367d290b2f2
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2025-08-14 07:22:32 +0000

    Hide profile avatar

--- Home.py
+++ Home.py
@@ -42,8 +42,7 @@ hide_streamlit_style = """
         display: none !important;
     }
     .stDeployButton {display: none !important;} /* Hides deploy button */
-    image[aria-label="App Creator Avatar"], 
-    link[aria-label="App Creator Avatar"] {
+    [aria-label="App Creator Avatar"] {
         display: none !important; /* Hides Sanaa Bhorkar profile button*
     }
     </style>
