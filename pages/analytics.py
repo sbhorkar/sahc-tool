8294ec4c2a77a0b2ff0397c86d7a71af6e0b577f
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2025-08-15 06:12:35 +0000

    Go back to old home page

--- pages/analytics.py
+++ pages/analytics.py
@@ -23,6 +23,15 @@ if not st.session_state.auth_ok:
 else:
     st.success("Authenticated")
 
+    st.write("Legend for each column:")
+    st.write("* user_id: random string set for each new 'viewer'"
+             "* shared_clicked: numbers of times that user clicked the 'Shared via...' button" \
+             "* interacted: true if user inputted a number for any marker; false if not"
+             "* thumbs_up: number of times the user clicked thumbs_up"
+             "* thummbs_down: number of times the user clicked thumbs_down"
+             "* first_*_at: date and time of the first time the user did the respective action"
+                   )
+
     if os.path.exists(DB_PATH):
         try:
             conn = sqlite3.connect(DB_PATH)
